bc250d456594c2aa514132eb14aa7ee2cc06944b
raphael hidalgo <raphael.hidalgo@ieseg.fr>
2022-08-29 17:13:54 +0200

    validations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :preference
   has_many :blocked_users
-  validates :first_name, :last_name, :birthdate, :description, :gender, :address, presence: true
+  validates :first_name, :last_name, :birthdate, :description, :gender, presence: true
   validates :description, length: { minimum: 100 }
   validate :old_enough?
 
